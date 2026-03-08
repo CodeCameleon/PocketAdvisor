12a6456bf38ebe0c2f38defd6dac5dec2a13ea25
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-08 20:34:06 +0100

    Created the user role enum.

---
+++ Backend/PocketAdvisor.Enums/EUserRole.cs
@@ -0,0 +1,17 @@
+﻿namespace PocketAdvisor.Enums;
+
+/// <summary>
+/// The enumeration containing the possible user roles in the system.
+/// </summary>
+public enum EUserRole
+{
+    /// <summary>
+    /// The administrator role, which is the highest level of access.
+    /// </summary>
+    Administrator = 1,
+    
+    /// <summary>
+    /// The regular user role, which is the default level of access.
+    /// </summary>
+    User = 2
+}
