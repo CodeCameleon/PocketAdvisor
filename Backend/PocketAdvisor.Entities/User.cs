9d993df7c132ccd201985609d4e9d33c14eeb3b5
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-08 20:46:13 +0100

    Created the user entity class.

---
+++ Backend/PocketAdvisor.Entities/User.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PocketAdvisor.Enums;
+
+namespace PocketAdvisor.Entities;
+
+/// <summary>
+/// The database entity representing a user in the system.
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// The unique identifier of the user.
+    /// </summary>
+    [Key]
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// The flag indicating whether the user's email address has been verified.
+    /// </summary>
+    public bool IsEmailVerified { get; set; }
+    
+    /// <summary>
+    /// The email address of the user.
+    /// </summary>
+    [Required]
+    [MaxLength(100)]
+    public required string Email { get; set; }
+    
+    /// <summary>
+    /// The hashed password of the user.
+    /// </summary>
+    [Required]
+    [MaxLength(150)]
+    public required string PasswordHash { get; set; }
+    
+    /// <summary>
+    /// The role of the user.
+    /// </summary>
+    public required EUserRole Role { get; set; }
+}
