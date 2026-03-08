70718446145e70ec42ce0b649e7ceebd6d71ae3e
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-08 21:42:34 +0100

    Created the database context class.

---
+++ Backend/PocketAdvisor.DbContexts/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PocketAdvisor.DbContexts.Extensions;
+
+/// <summary>
+/// The extensions methods for the <see cref="IServiceCollection" /> interface.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="PocketAdvisorDbContext" /> to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="connectionString">The connection string to the database.</param>
+    /// <returns>The service collection for chaining.</returns>
+    public static IServiceCollection AddPocketAdvisorDbContext(this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddDbContext<PocketAdvisorDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
+        
+        return services;
+    }
+}
---
+++ Backend/PocketAdvisor.DbContexts/PocketAdvisorDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PocketAdvisor.Entities;
+
+namespace PocketAdvisor.DbContexts;
+
+/// <summary>
+/// The database context for the project.
+/// </summary>
+public sealed class PocketAdvisorDbContext
+    : DbContext
+{
+    #region Constructors
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PocketAdvisorDbContext" /> class,
+    /// using the specified options.
+    /// </summary>
+    /// <param name="options">The options to be used by the context.</param>
+    public PocketAdvisorDbContext(DbContextOptions<PocketAdvisorDbContext> options) : base(options) { }
+    
+    #endregion
+    
+    #region DbSets
+    
+    /// <summary>
+    /// The database table for the token entities.
+    /// </summary>
+    public DbSet<Token> Tokens { get; set; }
+    
+    /// <summary>
+    /// The database table for the user entities.
+    /// </summary>
+    public DbSet<User> Users { get; set; }
+    
+    #endregion
+    
+    #region OnModelCreating
+    
+    /// <summary>
+    /// Configures the model that was discovered by convention from the entity types
+    /// exposed in <see cref="DbSet{TEntity}" /> properties on your derived context.
+    /// </summary>
+    /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Token>(entity =>
+        {
+            entity.HasIndex(t => t.Hash).IsUnique();
+            entity.HasOne(t => t.User)
+                .WithMany(u => u.Tokens)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+        
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
+    }
+    
+    #endregion
+}
