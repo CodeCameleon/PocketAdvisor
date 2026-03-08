37d5db062f1ca0d7db35b8a2e6c2720a4093d363
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-08 20:38:37 +0100

    Created the token type enum.

---
+++ Backend/PocketAdvisor.Enums/ETokenType.cs
@@ -0,0 +1,22 @@
+﻿namespace PocketAdvisor.Enums;
+
+/// <summary>
+/// The enumeration containing the possible token types in the system.
+/// </summary>
+public enum ETokenType
+{
+    /// <summary>
+    /// The email verification token type.
+    /// </summary>
+    EmailVerification = 1,
+    
+    /// <summary>
+    /// The password reset token type.
+    /// </summary>
+    PasswordReset = 2,
+    
+    /// <summary>
+    /// The refresh token type.
+    /// </summary>
+    Refresh = 3
+}
