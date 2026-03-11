19e9f9a7816d598bef2e8b00cd51d1313b6354b6
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-11 17:21:35 +0100

    Created the currency code enum.

---
+++ Backend/PocketAdvisor.Enums/ECurrencyCode.cs
@@ -0,0 +1,772 @@
+﻿namespace PocketAdvisor.Enums;
+
+/// <summary>
+/// The enumeration containing the possible currency codes in the system.
+/// </summary>
+public enum ECurrencyCode
+{
+    /// <summary>
+    /// The Albanian lek currency code.
+    /// </summary>
+    All = 8,
+    
+    /// <summary>
+    /// The Algerian dinar currency code.
+    /// </summary>
+    Dzd = 12,
+    
+    /// <summary>
+    /// The Argentine peso currency code.
+    /// </summary>
+    Ars = 32,
+    
+    /// <summary>
+    /// The Australian dollar currency code.
+    /// </summary>
+    Aud = 36,
+    
+    /// <summary>
+    /// The Bahamian dollar currency code.
+    /// </summary>
+    Bsd = 44,
+    
+    /// <summary>
+    /// The Bahraini dinar currency code.
+    /// </summary>
+    Bhd = 48,
+    
+    /// <summary>
+    /// The Bangladeshi taka currency code.
+    /// </summary>
+    Bdt = 50,
+    
+    /// <summary>
+    /// The Armenian dram currency code.
+    /// </summary>
+    Amd = 51,
+    
+    /// <summary>
+    /// The Barbados dollar currency code.
+    /// </summary>
+    Bbd = 52,
+    
+    /// <summary>
+    /// The Bermudian dollar currency code.
+    /// </summary>
+    Bmd = 60,
+    
+    /// <summary>
+    /// The Bhutanese ngultrum currency code.
+    /// </summary>
+    Btn = 64,
+    
+    /// <summary>
+    /// The Boliviano currency code.
+    /// </summary>
+    Bob = 68,
+    
+    /// <summary>
+    /// The Botswana pula currency code.
+    /// </summary>
+    Bwp = 72,
+    
+    /// <summary>
+    /// The Belize dollar currency code.
+    /// </summary>
+    Bzd = 84,
+    
+    /// <summary>
+    /// The Solomon Islands dollar currency code.
+    /// </summary>
+    Sbd = 90,
+    
+    /// <summary>
+    /// The Brunei dollar currency code.
+    /// </summary>
+    Bnd = 96,
+    
+    /// <summary>
+    /// The Myanmar kyat currency code.
+    /// </summary>
+    Mmk = 104,
+    
+    /// <summary>
+    /// The Burundian franc currency code.
+    /// </summary>
+    Bif = 108,
+    
+    /// <summary>
+    /// The Cambodian riel currency code.
+    /// </summary>
+    Khr = 116,
+    
+    /// <summary>
+    /// The Canadian dollar currency code.
+    /// </summary>
+    Cad = 124,
+    
+    /// <summary>
+    /// The Cape Verdean escudo currency code.
+    /// </summary>
+    Cve = 132,
+    
+    /// <summary>
+    /// The Cayman Islands dollar currency code.
+    /// </summary>
+    Kyd = 136,
+    
+    /// <summary>
+    /// The Sri Lankan rupee currency code.
+    /// </summary>
+    Lkr = 144,
+    
+    /// <summary>
+    /// The Chilean peso currency code.
+    /// </summary>
+    Clp = 152,
+    
+    /// <summary>
+    /// The Renminbi currency code.
+    /// </summary>
+    Cny = 156,
+    
+    /// <summary>
+    /// The Colombian peso currency code.
+    /// </summary>
+    Cop = 170,
+    
+    /// <summary>
+    /// The Comoro franc currency code.
+    /// </summary>
+    Kmf = 174,
+    
+    /// <summary>
+    /// The Costa Rican colon currency code.
+    /// </summary>
+    Crc = 188,
+    
+    /// <summary>
+    /// The Cuban peso currency code.
+    /// </summary>
+    Cup = 192,
+    
+    /// <summary>
+    /// The Czech koruna currency code.
+    /// </summary>
+    Czk = 203,
+    
+    /// <summary>
+    /// The Danish krone currency code.
+    /// </summary>
+    Dkk = 208,
+    
+    /// <summary>
+    /// The Dominican peso currency code.
+    /// </summary>
+    Dop = 214,
+    
+    /// <summary>
+    /// The Salvadoran colón currency code.
+    /// </summary>
+    Svc = 222,
+    
+    /// <summary>
+    /// The Ethiopian birr currency code.
+    /// </summary>
+    Etb = 230,
+    
+    /// <summary>
+    /// The Eritrean nakfa currency code.
+    /// </summary>
+    Ern = 232,
+    
+    /// <summary>
+    /// The Falkland Islands pound currency code.
+    /// </summary>
+    Fkp = 238,
+    
+    /// <summary>
+    /// The Fiji dollar currency code.
+    /// </summary>
+    Fjd = 242,
+    
+    /// <summary>
+    /// The Djiboutian franc currency code.
+    /// </summary>
+    Djf = 262,
+    
+    /// <summary>
+    /// The Gambian dalasi currency code.
+    /// </summary>
+    Gmd = 270,
+    
+    /// <summary>
+    /// The Gibraltar pound currency code.
+    /// </summary>
+    Gip = 292,
+    
+    /// <summary>
+    /// The Guatemalan quetzal currency code.
+    /// </summary>
+    Gtq = 320,
+    
+    /// <summary>
+    /// The Guinean franc currency code.
+    /// </summary>
+    Gnf = 324,
+    
+    /// <summary>
+    /// The Guyanese dollar currency code.
+    /// </summary>
+    Gyd = 328,
+    
+    /// <summary>
+    /// The Haitian gourde currency code.
+    /// </summary>
+    Htg = 332,
+    
+    /// <summary>
+    /// The Honduran lempira currency code.
+    /// </summary>
+    Hnl = 340,
+    
+    /// <summary>
+    /// The Hong Kong dollar currency code.
+    /// </summary>
+    Hkd = 344,
+    
+    /// <summary>
+    /// The Hungarian forint currency code.
+    /// </summary>
+    Huf = 348,
+    
+    /// <summary>
+    /// The Icelandic króna currency code.
+    /// </summary>
+    Isk = 352,
+    
+    /// <summary>
+    /// The Indian rupee currency code.
+    /// </summary>
+    Inr = 356,
+    
+    /// <summary>
+    /// The Indonesian rupiah currency code.
+    /// </summary>
+    Idr = 360,
+    
+    /// <summary>
+    /// The Iranian rial currency code.
+    /// </summary>
+    Irr = 364,
+    
+    /// <summary>
+    /// The Iraqi dinar currency code.
+    /// </summary>
+    Iqd = 368,
+    
+    /// <summary>
+    /// The Israeli new shekel currency code.
+    /// </summary>
+    Ils = 376,
+    
+    /// <summary>
+    /// The Jamaican dollar currency code.
+    /// </summary>
+    Jmd = 388,
+    
+    /// <summary>
+    /// The Japanese yen currency code.
+    /// </summary>
+    Jpy = 392,
+    
+    /// <summary>
+    /// The Kazakhstani tenge currency code.
+    /// </summary>
+    Kzt = 398,
+    
+    /// <summary>
+    /// The Jordanian dinar currency code.
+    /// </summary>
+    Jod = 400,
+    
+    /// <summary>
+    /// The Kenyan shilling currency code.
+    /// </summary>
+    Kes = 404,
+    
+    /// <summary>
+    /// The North Korean won currency code.
+    /// </summary>
+    Kpw = 408,
+    
+    /// <summary>
+    /// The South Korean won currency code.
+    /// </summary>
+    Krw = 410,
+    
+    /// <summary>
+    /// The Kuwaiti dinar currency code.
+    /// </summary>
+    Kwd = 414,
+    
+    /// <summary>
+    /// The Kyrgyzstani som currency code.
+    /// </summary>
+    Kgs = 417,
+    
+    /// <summary>
+    /// The Lao kip currency code.
+    /// </summary>
+    Lak = 418,
+    
+    /// <summary>
+    /// The Lebanese pound currency code.
+    /// </summary>
+    Lbp = 422,
+    
+    /// <summary>
+    /// The Lesotho loti currency code.
+    /// </summary>
+    Lsl = 426,
+    
+    /// <summary>
+    /// The Liberian dollar currency code.
+    /// </summary>
+    Lrd = 430,
+    
+    /// <summary>
+    /// The Libyan dinar currency code.
+    /// </summary>
+    Lyd = 434,
+    
+    /// <summary>
+    /// The Macanese pataca currency code.
+    /// </summary>
+    Mop = 446,
+    
+    /// <summary>
+    /// The Malawian kwacha currency code.
+    /// </summary>
+    Mwk = 454,
+    
+    /// <summary>
+    /// The Malaysian ringgit currency code.
+    /// </summary>
+    Myr = 458,
+    
+    /// <summary>
+    /// The Maldivian rufiyaa currency code.
+    /// </summary>
+    Mvr = 462,
+    
+    /// <summary>
+    /// The Mauritian rupee currency code.
+    /// </summary>
+    Mur = 480,
+    
+    /// <summary>
+    /// The Mexican peso currency code.
+    /// </summary>
+    Mxn = 484,
+    
+    /// <summary>
+    /// The Mongolian tögrög currency code.
+    /// </summary>
+    Mnt = 496,
+    
+    /// <summary>
+    /// The Moldovan leu currency code.
+    /// </summary>
+    Mdl = 498,
+    
+    /// <summary>
+    /// The Moroccan dirham currency code.
+    /// </summary>
+    Mad = 504,
+    
+    /// <summary>
+    /// The Omani rial currency code.
+    /// </summary>
+    Omr = 512,
+    
+    /// <summary>
+    /// The Namibian dollar currency code.
+    /// </summary>
+    Nad = 516,
+    
+    /// <summary>
+    /// The Nepalese rupee currency code.
+    /// </summary>
+    Npr = 524,
+    
+    /// <summary>
+    /// The Caribbean guilder currency code.
+    /// </summary>
+    Xcg = 532,
+    
+    /// <summary>
+    /// The Aruban florin currency code.
+    /// </summary>
+    Awg = 533,
+    
+    /// <summary>
+    /// The Vanuatu vatu currency code.
+    /// </summary>
+    Vuv = 548,
+    
+    /// <summary>
+    /// The New Zealand dollar currency code.
+    /// </summary>
+    Nzd = 554,
+    
+    /// <summary>
+    /// The Nicaraguan córdoba currency code.
+    /// </summary>
+    Nio = 558,
+    
+    /// <summary>
+    /// The Nigerian naira currency code.
+    /// </summary>
+    Ngn = 566,
+    
+    /// <summary>
+    /// The Norwegian krone currency code.
+    /// </summary>
+    Nok = 578,
+    
+    /// <summary>
+    /// The Pakistani rupee currency code.
+    /// </summary>
+    Pkr = 586,
+    
+    /// <summary>
+    /// The Panamanian balboa currency code.
+    /// </summary>
+    Pab = 590,
+    
+    /// <summary>
+    /// The Papua New Guinean kina currency code.
+    /// </summary>
+    Pgk = 598,
+    
+    /// <summary>
+    /// The Paraguayan guaraní currency code.
+    /// </summary>
+    Pyg = 600,
+    
+    /// <summary>
+    /// The Peruvian sol currency code.
+    /// </summary>
+    Pen = 604,
+    
+    /// <summary>
+    /// The Philippine peso currency code.
+    /// </summary>
+    Php = 608,
+    
+    /// <summary>
+    /// The Qatari riyal currency code.
+    /// </summary>
+    Qar = 634,
+    
+    /// <summary>
+    /// The Russian ruble currency code.
+    /// </summary>
+    Rub = 643,
+    
+    /// <summary>
+    /// The Rwandan franc currency code.
+    /// </summary>
+    Rwf = 646,
+    
+    /// <summary>
+    /// The Saint Helena pound currency code.
+    /// </summary>
+    Shp = 654,
+    
+    /// <summary>
+    /// The Saudi riyal currency code.
+    /// </summary>
+    Sar = 682,
+    
+    /// <summary>
+    /// The Seychelles rupee currency code.
+    /// </summary>
+    Scr = 690,
+    
+    /// <summary>
+    /// The Singapore dollar currency code.
+    /// </summary>
+    Sgd = 702,
+    
+    /// <summary>
+    /// The Vietnamese đồng currency code.
+    /// </summary>
+    Vnd = 704,
+    
+    /// <summary>
+    /// The Somalian shilling currency code.
+    /// </summary>
+    Sos = 706,
+    
+    /// <summary>
+    /// The South African rand currency code.
+    /// </summary>
+    Zar = 710,
+    
+    /// <summary>
+    /// The Swazi lilangeni currency code.
+    /// </summary>
+    Szl = 748,
+    
+    /// <summary>
+    /// The Swedish krona currency code.
+    /// </summary>
+    Sek = 752,
+    
+    /// <summary>
+    /// The Swiss franc currency code.
+    /// </summary>
+    Chf = 756,
+    
+    /// <summary>
+    /// The Syrian pound currency code.
+    /// </summary>
+    Syp = 760,
+    
+    /// <summary>
+    /// The Thai baht currency code.
+    /// </summary>
+    Thb = 764,
+    
+    /// <summary>
+    /// The Tongan paʻanga currency code.
+    /// </summary>
+    Top = 776,
+    
+    /// <summary>
+    /// The Trinidad and Tobago dollar currency code.
+    /// </summary>
+    Ttd = 780,
+    
+    /// <summary>
+    /// The United Arab Emirates dirham currency code.
+    /// </summary>
+    Aed = 784,
+    
+    /// <summary>
+    /// The Tunisian dinar currency code.
+    /// </summary>
+    Tnd = 788,
+    
+    /// <summary>
+    /// The Ugandan shilling currency code.
+    /// </summary>
+    Ugx = 800,
+    
+    /// <summary>
+    /// The Macedonian denar currency code.
+    /// </summary>
+    Mkd = 807,
+    
+    /// <summary>
+    /// The Egyptian pound currency code.
+    /// </summary>
+    Egp = 818,
+    
+    /// <summary>
+    /// The Pound sterling currency code.
+    /// </summary>
+    Gbp = 826,
+    
+    /// <summary>
+    /// The Tanzanian shilling currency code.
+    /// </summary>
+    Tzs = 834,
+    
+    /// <summary>
+    /// The United States dollar currency code.
+    /// </summary>
+    Usd = 840,
+    
+    /// <summary>
+    /// The Uruguayan peso currency code.
+    /// </summary>
+    Uyu = 858,
+    
+    /// <summary>
+    /// The Uzbekistan sum currency code.
+    /// </summary>
+    Uzs = 860,
+    
+    /// <summary>
+    /// The Samoan tala currency code.
+    /// </summary>
+    Wst = 882,
+    
+    /// <summary>
+    /// The Yemeni rial currency code.
+    /// </summary>
+    Yer = 886,
+    
+    /// <summary>
+    /// The New Taiwan dollar currency code.
+    /// </summary>
+    Twd = 901,
+    
+    /// <summary>
+    /// The Zimbabwe Gold currency code.
+    /// </summary>
+    Zwg = 924,
+    
+    /// <summary>
+    /// The Sierra Leonean leone currency code.
+    /// </summary>
+    Sle = 925,
+    
+    /// <summary>
+    /// The Venezuelan sovereign bolívar currency code.
+    /// </summary>
+    Ves = 928,
+    
+    /// <summary>
+    /// The Mauritanian ouguiya currency code.
+    /// </summary>
+    Mru = 929,
+    
+    /// <summary>
+    /// The São Tomé and Príncipe dobra currency code.
+    /// </summary>
+    Stn = 930,
+    
+    /// <summary>
+    /// The Belarusian ruble currency code.
+    /// </summary>
+    Byn = 933,
+    
+    /// <summary>
+    /// The Turkmenistan manat currency code.
+    /// </summary>
+    Tmt = 934,
+    
+    /// <summary>
+    /// The Ghanaian cedi currency code.
+    /// </summary>
+    Ghs = 936,
+    
+    /// <summary>
+    /// The Sudanese pound currency code.
+    /// </summary>
+    Sdg = 938,
+    
+    /// <summary>
+    /// The Serbian dinar currency code.
+    /// </summary>
+    Rsd = 941,
+    
+    /// <summary>
+    /// The Mozambican metical currency code.
+    /// </summary>
+    Mzn = 943,
+    
+    /// <summary>
+    /// The Azerbaijani manat currency code.
+    /// </summary>
+    Azn = 944,
+    
+    /// <summary>
+    /// The Romanian leu currency code.
+    /// </summary>
+    Ron = 946,
+    
+    /// <summary>
+    /// The Turkish lira currency code.
+    /// </summary>
+    Try = 949,
+    
+    /// <summary>
+    /// The CFA franc BEAC currency code.
+    /// </summary>
+    Xaf = 950,
+    
+    /// <summary>
+    /// The East Caribbean dollar currency code.
+    /// </summary>
+    Xcd = 951,
+    
+    /// <summary>
+    /// The CFA franc BCEAO currency code.
+    /// </summary>
+    Xof = 952,
+    
+    /// <summary>
+    /// The CFP franc currency code.
+    /// </summary>
+    Xpf = 953,
+    
+    /// <summary>
+    /// The Zambian kwacha currency code.
+    /// </summary>
+    Zmw = 967,
+    
+    /// <summary>
+    /// The Surinamese dollar currency code.
+    /// </summary>
+    Srd = 968,
+    
+    /// <summary>
+    /// The Malagasy ariary currency code.
+    /// </summary>
+    Mga = 969,
+    
+    /// <summary>
+    /// The Afghan afghani currency code.
+    /// </summary>
+    Afn = 971,
+    
+    /// <summary>
+    /// The Tajikistani somoni currency code.
+    /// </summary>
+    Tjs = 972,
+    
+    /// <summary>
+    /// The Angolan kwanza currency code.
+    /// </summary>
+    Aoa = 973,
+    
+    /// <summary>
+    /// The Congolese franc currency code.
+    /// </summary>
+    Cdf = 976,
+    
+    /// <summary>
+    /// The Bosnia and Herzegovina convertible mark currency code.
+    /// </summary>
+    Bam = 977,
+    
+    /// <summary>
+    /// The Euro currency code.
+    /// </summary>
+    Eur = 978,
+    
+    /// <summary>
+    /// The Ukrainian hryvnia currency code.
+    /// </summary>
+    Uah = 980,
+    
+    /// <summary>
+    /// The Georgian lari currency code.
+    /// </summary>
+    Gel = 981,
+    
+    /// <summary>
+    /// The Polish złoty currency code.
+    /// </summary>
+    Pln = 985,
+    
+    /// <summary>
+    /// The Brazilian real currency code.
+    /// </summary>
+    Brl = 986
+}
