0bf11416b095c9284ea258b29f50705fe641ec85
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-12 19:45:55 +0100

    Implemented the extension methods for the currency code enum.

---
+++ Backend/PocketAdvisor.Enums/Extensions/ECurrencyCodeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace PocketAdvisor.Enums.Extensions;
+
+/// <summary>
+/// The extensions methods for the <see cref="ECurrencyCode" /> enum.
+/// </summary>
+public static class ECurrencyCodeExtensions
+{
+    /// <summary>
+    /// Gets the standard ISO 4217 code of the currency code.
+    /// </summary>
+    /// <param name="currencyCode">The currency code.</param>
+    /// <returns>The ISO 4217 code of the currency code.</returns>
+    public static string GetIsoCode(this ECurrencyCode currencyCode)
+    {
+        return currencyCode.ToString().ToUpper();
+    }
+}
