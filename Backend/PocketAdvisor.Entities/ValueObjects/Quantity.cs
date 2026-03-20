7b5294c074f3600bd0ef6f0ec309589f5bb02db3
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-03-20 15:04:04 +0100

    Finished the quantity class.

--- Backend/PocketAdvisor.Entities/ValueObjects/Quantity.cs
+++ Backend/PocketAdvisor.Entities/ValueObjects/Quantity.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using PocketAdvisor.Enums;
 using PocketAdvisor.Enums.Extensions;
 
@@ -7,22 +8,37 @@ namespace PocketAdvisor.Entities.ValueObjects;
 /// <summary>
 /// A value object representing a quantity, which consists of a value and a unit.
 /// </summary>
-public readonly struct Quantity
-    : IEquatable<Quantity>
+public sealed class Quantity
+    : IComparable<Quantity>, IEquatable<Quantity>
 {
     #region Constants
+    
+    /// <summary>
+    /// When the comparison is not possible because the units belong to different categories.
+    /// </summary>
+    private const string CategoryMismatchMessage = "Units must belong to the same category.";
     
     /// <summary>
     /// The template used to stringify the object.
     /// </summary>
     private const string FormatTemplate = "{0} {1}";
     
+    /// <summary>
+    /// When the comparison is not possible because one of the units belongs to the uncategorized category.
+    /// </summary>
+    private const string UncategorizedMessage = "Uncategorized units cannot be compared.";
+    
+    /// <summary>
+    /// The number of decimal places used when normalizing values for comparison and hashing.
+    /// </summary>
+    private const int Precision = 6;
+    
     #endregion
     
     #region Constructors
     
     /// <summary>
-    /// Initializes a new instance of the <see cref="Quantity" /> struct,
+    /// Initializes a new instance of the <see cref="Quantity" /> class,
     /// with the given value and unit.
     /// </summary>
     /// <param name="value">The value of the quantity.</param>
@@ -52,12 +68,80 @@ public readonly struct Quantity
     #region Operators
     
     /// <summary>
+    /// Checks if the first quantity is less than the second quantity.
+    /// </summary>
+    /// <param name="left">The first quantity.</param>
+    /// <param name="right">The second quantity.</param>
+    /// <returns>
+    /// <see langword="true" />, if the first quantity is less than
+    /// the second quantity, <see langword="false" /> otherwise.
+    /// </returns>
+    public static bool operator <(Quantity? left, Quantity? right)
+    {
+        if (left is null)
+        {
+            return right is not null;
+        }
+        
+        return left.CompareTo(right) < 0;
+    }
+    
+    /// <summary>
+    /// Checks if the first quantity is greater than the second quantity.
+    /// </summary>
+    /// <param name="left">The first quantity.</param>
+    /// <param name="right">The second quantity.</param>
+    /// <returns>
+    /// <see langword="true" />, if the first quantity is greater than
+    /// the second quantity, <see langword="false" /> otherwise.
+    /// </returns>
+    public static bool operator >(Quantity? left, Quantity? right)
+    {
+        if (left is null)
+        {
+            return false;
+        }
+        
+        return left.CompareTo(right) > 0;
+    }
+    
+    /// <summary>
+    /// Checks if the first quantity is less than or equal to the second quantity.
+    /// </summary>
+    /// <param name="left">The first quantity.</param>
+    /// <param name="right">The second quantity.</param>
+    /// <returns>
+    /// <see langword="true" />, if the first quantity is less than or
+    /// equal to the second quantity, <see langword="false" /> otherwise.
+    /// </returns>
+    public static bool operator <=(Quantity? left, Quantity? right) => !(left > right);
+    
+    /// <summary>
+    /// Checks if the first quantity is greater than or equal to the second quantity.
+    /// </summary>
+    /// <param name="left">The first quantity.</param>
+    /// <param name="right">The second quantity.</param>
+    /// <returns>
+    /// <see langword="true" />, if the first quantity is greater than or
+    /// equal to the second quantity, <see langword="false" /> otherwise.
+    /// </returns>
+    public static bool operator >=(Quantity? left, Quantity? right) => !(left < right);
+    
+    /// <summary>
     /// Checks if two quantities are equal.
     /// </summary>
     /// <param name="left">The first quantity.</param>
     /// <param name="right">The second quantity.</param>
     /// <returns><see langword="true" />, if they are equal, <see langword="false" /> otherwise.</returns>
-    public static bool operator ==(Quantity left, Quantity right) => left.Equals(right);
+    public static bool operator ==(Quantity? left, Quantity? right)
+    {
+        if (left is null)
+        {
+            return right is null;
+        }
+        
+        return left.Equals(right);
+    }
     
     /// <summary>
     /// Checks if two quantities are not equal.
@@ -65,15 +149,58 @@ public readonly struct Quantity
     /// <param name="left">The first quantity.</param>
     /// <param name="right">The second quantity.</param>
     /// <returns><see langword="true" />, if they are not equal, <see langword="false" /> otherwise.</returns>
-    public static bool operator !=(Quantity left, Quantity right) => !left.Equals(right);
+    public static bool operator !=(Quantity? left, Quantity? right) => !(left == right);
+    
+    #endregion
+    
+    #region CompareTo
+    
+    /// <inheritdoc />
+    /// <exception cref="InvalidOperationException">
+    /// If the units belong to different categories or<br />
+    /// if one of the units belongs to the uncategorized category.
+    /// </exception>
+    public int CompareTo(Quantity? other)
+    {
+        if (other is null)
+        {
+            return 1;
+        }
+        
+        if (Unit == other.Unit)
+        {
+            return Value.CompareTo(other.Value);
+        }
+        
+        EUnitCategory category = Unit.GetUnitCategory();
+        EUnitCategory otherCategory = other.Unit.GetUnitCategory();
+        
+        if (category == EUnitCategory.Uncategorized || otherCategory == EUnitCategory.Uncategorized)
+        {
+            throw new InvalidOperationException(UncategorizedMessage);
+        }
+        
+        if (category != otherCategory)
+        {
+            throw new InvalidOperationException(CategoryMismatchMessage);
+        }
+        
+        decimal factor = Unit.GetUnitFactor(other.Unit);
+        return (Value * factor).CompareTo(other.Value);
+    }
     
     #endregion
     
     #region Equals
     
     /// <inheritdoc />
-    public bool Equals(Quantity other)
+    public bool Equals(Quantity? other)
     {
+        if (other is null)
+        {
+            return false;
+        }
+        
         if (Unit == other.Unit)
         {
             return Value == other.Value;
@@ -82,19 +209,18 @@ public readonly struct Quantity
         EUnitCategory category = Unit.GetUnitCategory();
         EUnitCategory otherCategory = other.Unit.GetUnitCategory();
         
-        if (category == EUnitCategory.Uncategorized || otherCategory == EUnitCategory.Uncategorized)
-        {
-            return false;
-        }
-        
-        if (category != otherCategory)
+        if (category == EUnitCategory.Uncategorized ||
+            otherCategory == EUnitCategory.Uncategorized ||
+            category != otherCategory)
         {
             return false;
         }
         
         decimal factor = Unit.GetUnitFactor(other.Unit);
+        decimal normalized = Math.Round(Value * factor, Precision);
+        decimal otherValue = Math.Round(other.Value, Precision);
         
-        return Value * factor == other.Value;
+        return normalized == otherValue;
     }
     
     /// <inheritdoc />
@@ -125,7 +251,8 @@ public readonly struct Quantity
         }
         
         decimal factor = Unit.GetUnitFactor(baseUnit);
-        return HashCode.Combine(Value * factor, baseUnit);
+        decimal normalized = Math.Round(Value * factor, Precision);
+        return HashCode.Combine(normalized, baseUnit);
     }
     
     #endregion
@@ -135,7 +262,7 @@ public readonly struct Quantity
     /// <inheritdoc />
     public override string ToString()
     {
-        return string.Format(FormatTemplate, Value, Unit);
+        return string.Format(CultureInfo.InvariantCulture, FormatTemplate, Value, Unit);
     }
     
     #endregion
