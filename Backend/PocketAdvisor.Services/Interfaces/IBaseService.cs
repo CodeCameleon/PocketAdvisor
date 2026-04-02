0cebde80dc98d51747047ce93a04d505bb751465
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-02 15:06:07 +0200

    Defined the base service interface.

---
+++ Backend/PocketAdvisor.Services/Interfaces/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿namespace PocketAdvisor.Services.Interfaces;
+
+/// <summary>
+/// Defines the base service interface for all services.
+/// </summary>
+public interface IBaseService
+{
+    // This is just a marker interface.
+}
