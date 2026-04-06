e8f8d5c508c33d0dc1b515a4215bed84c0285323
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-06 22:54:36 +0200

    Created the create user request class.

---
+++ Backend/PocketAdvisor.Requests/Users/CreateUserRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace PocketAdvisor.Requests.Users;
+
+/// <summary>
+/// The request model for creating a new user in the system.
+/// </summary>
+public sealed class CreateUserRequest
+{
+    /// <summary>
+    /// The email address of the user.
+    /// </summary>
+    public string? Email { get; set; }
+    
+    /// <summary>
+    /// The password of the user.
+    /// </summary>
+    public string? Password { get; set; }
+    
+    /// <summary>
+    /// The confirmation of the password.
+    /// </summary>
+    public string? ConfirmPassword { get; set; }
+}
