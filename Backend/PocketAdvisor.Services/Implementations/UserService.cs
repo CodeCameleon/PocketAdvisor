693bc5bd0fc2c2bbe4f55d4120cb1cc64e60e44b
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-08 11:19:01 +0200

    Created the user service class.

---
+++ Backend/PocketAdvisor.Services/Implementations/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using PocketAdvisor.Services.Interfaces;
+
+namespace PocketAdvisor.Services.Implementations;
+
+/// <summary>
+/// Represents the service implementation for performing operations related to users.
+/// </summary>
+public sealed class UserService
+    : BaseService<UserService>, IUserService
+{
+    #region Constructors
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserService" /> class.
+    /// </summary>
+    /// <param name="logger">The logger for the class.</param>
+    /// <param name="serviceProvider">The service provider for resolving dependencies.</param>
+    public UserService(ILogger<UserService> logger, IServiceProvider serviceProvider)
+        : base(logger, serviceProvider)
+    {
+        
+    }
+    
+    #endregion
+}
