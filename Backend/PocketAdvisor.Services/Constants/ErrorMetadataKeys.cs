03f1f61a7b414ef46dceae1918cfb90f797f68d6
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-08 16:46:53 +0200

    Implemented the extension method for converting the validation failures.

---
+++ Backend/PocketAdvisor.Services/Constants/ErrorMetadataKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace PocketAdvisor.Services.Constants;
+
+/// <summary>
+/// Represents the metadata keys used for error handling in the services.
+/// </summary>
+public static class ErrorMetadataKeys
+{
+    /// <summary>
+    /// The metadata key that stores the name of the property.
+    /// </summary>
+    public const string PropertyName = "PropertyName";
+}
---
+++ Backend/PocketAdvisor.Services/Extensions/ValidationFailureExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FluentResults;
+using FluentValidation.Results;
+using PocketAdvisor.Services.Constants;
+
+namespace PocketAdvisor.Services.Extensions;
+
+/// <summary>
+/// The extension methods for the <see cref="ValidationFailure" /> class.
+/// </summary>
+internal static class ValidationFailureExtensions
+{
+    /// <summary>
+    /// Converts the validation failures to returnable errors.
+    /// </summary>
+    /// <param name="validationFailures">The validation failure list to convert.</param>
+    /// <returns>The ready to use error list.</returns>
+    internal static IReadOnlyList<Error> ToErrorList(this IEnumerable<ValidationFailure> validationFailures)
+    {
+        return validationFailures.Select(failure => new Error(failure.ErrorMessage)
+        {
+            Metadata =
+            {
+                [ErrorMetadataKeys.PropertyName] = failure.PropertyName
+            }
+        }).ToList();
+    }
+}
