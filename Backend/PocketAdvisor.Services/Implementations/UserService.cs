5e0551a905dff216995c01f495135b90568ce5a5
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-10 23:09:00 +0200

    Implemented the create user service method.

--- Backend/PocketAdvisor.Services/Implementations/UserService.cs
+++ Backend/PocketAdvisor.Services/Implementations/UserService.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Security.Cryptography;
+using System.Text;
+using FluentResults;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PocketAdvisor.Entities;
+using PocketAdvisor.Enums;
 using PocketAdvisor.Repositories.Interfaces;
+using PocketAdvisor.Requests.Users;
 using PocketAdvisor.Services.Configurations;
+using PocketAdvisor.Services.Extensions;
 using PocketAdvisor.Services.Interfaces;
 
 namespace PocketAdvisor.Services.Implementations;
@@ -14,6 +22,26 @@ namespace PocketAdvisor.Services.Implementations;
 public sealed class UserService
     : BaseService<UserService>, IUserService
 {
+    #region GeneratedToken
+    
+    /// <summary>
+    /// Represents a generated token with its plain and hash values.
+    /// </summary>
+    /// <param name="Plain">The plain value of the token to send back.</param>
+    /// <param name="Hash">The hash value of the token to store.</param>
+    private record GeneratedToken(string Plain, string Hash);
+    
+    #endregion
+    
+    #region Constants
+    
+    /// <summary>
+    /// The size of the tokens in bytes.
+    /// </summary>
+    private const int TokenSize = 32;
+    
+    #endregion
+    
     #region Constructors
     
     /// <summary>
@@ -69,6 +97,81 @@ public sealed class UserService
     /// The user repository instance.
     /// </summary>
     private IUserRepository UserRepository { get; }
+    
+    #endregion
+    
+    #region GenerateToken
+    
+    /// <summary>
+    /// Generates a token using HMACSHA256 with the provided secret.
+    /// </summary>
+    /// <param name="secret">The secret to use for hashing.</param>
+    /// <returns>The created token.</returns>
+    private static GeneratedToken GenerateToken(string secret)
+    {
+        string plain = Convert.ToBase64String(RandomNumberGenerator.GetBytes(TokenSize));
+        
+        using HMACSHA256 hmacSha256 = new(Encoding.UTF8.GetBytes(secret));
+        byte[] hashBytes = hmacSha256.ComputeHash(Encoding.UTF8.GetBytes(plain));
+        
+        return new(plain, Convert.ToBase64String(hashBytes));
+    }
+    
+    #endregion
+    
+    #region CreateUserAsync
+    
+    /// <inheritdoc />
+    public async Task<Result<string>> CreateUserAsync(CreateUserRequest request)
+    {
+        Logger.LogInformation("Creating new user...");
+        
+        IValidator<CreateUserRequest> validator = GetValidator<CreateUserRequest>();
+        ValidationResult validationResult = await validator.ValidateAsync(request);
+        
+        if (!validationResult.IsValid)
+        {
+            if (Logger.IsEnabled(LogLevel.Warning))
+            {
+                Logger.LogWarning(
+                    "Validation failed for CreateUserRequest: {Errors}",
+                    validationResult.Errors
+                );
+            }
+            
+            return Result.Fail(validationResult.Errors.ToErrorList());
+        }
+        
+        await TransactionManager.Value.BeginTransactionAsync();
+        
+        User user = new()
+        {
+            Email = request.Email!,
+            PasswordHash = string.Empty,
+            Role = EUserRole.User
+        };
+        user.PasswordHash = PasswordHasher.HashPassword(user, request.Password);
+        
+        await UserRepository.CreateAsync(user);
+        
+        await TransactionManager.Value.SaveChangesAsync();
+        
+        GeneratedToken generatedToken = GenerateToken(TokenSecretsOptions.Value.EmailVerification);
+        
+        Token token = new()
+        {
+            Hash = generatedToken.Hash,
+            ExpiryAt = DateTime.UtcNow.AddHours(1),
+            Type = ETokenType.EmailVerification,
+            UserId = user.Id
+        };
+        await TokenRepository.CreateAsync(token);
+        
+        await TransactionManager.Value.CommitTransactionAsync();
+        
+        Logger.LogInformation("New user created successfully.");
+        return Result.Ok(generatedToken.Plain);
+    }
     
     #endregion
 }
--- Backend/PocketAdvisor.Services/Interfaces/IUserService.cs
+++ Backend/PocketAdvisor.Services/Interfaces/IUserService.cs
@@ -1,4 +1,7 @@
-﻿namespace PocketAdvisor.Services.Interfaces;
+﻿using FluentResults;
+using PocketAdvisor.Requests.Users;
+
+namespace PocketAdvisor.Services.Interfaces;
 
 /// <summary>
 /// Defines the service interface for performing operations related to users.
@@ -6,5 +9,14 @@
 public interface IUserService
     : IBaseService
 {
-    
+    /// <summary>
+    /// Creates a new user in the system asynchronously.
+    /// </summary>
+    /// <param name="request">The data of the user to create.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains a
+    /// <see cref="Result" /> indicating the success or failure of the operation.
+    /// If successful, the result contains the email verification token of the new user.
+    /// </returns>
+    Task<Result<string>> CreateUserAsync(CreateUserRequest request);
 }
