819984d648c2ffa47282cbbab6ce2ddb11ca59c3
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-17 23:56:09 +0200

    Overloaded the BadRequest method.

--- Backend/PocketAdvisor.WebApplication/Controllers/BaseController.cs
+++ Backend/PocketAdvisor.WebApplication/Controllers/BaseController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PocketAdvisor.Services.Constants;
 using PocketAdvisor.Services.Interfaces;
 
 namespace PocketAdvisor.WebApplication.Controllers;
@@ -37,6 +41,46 @@ public abstract class BaseController<TService>
     /// The service to be used by the controller.
     /// </summary>
     protected TService Service { get; }
+    
+    #endregion
+    
+    #region BadRequest
+    
+    /// <summary>
+    /// Creates a <see cref="BadRequestObjectResult" /> that produces a status code 400 response.
+    /// </summary>
+    /// <param name="errors">The FluentResults errors to return to the client.</param>
+    /// <returns>The created <see cref="BadRequestObjectResult" /> for the response.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If the given errors parameter is <see langword="null" />.
+    /// </exception>
+    protected BadRequestObjectResult BadRequest(IReadOnlyList<IError> errors)
+    {
+        ArgumentNullException.ThrowIfNull(errors);
+        
+        ProblemDetailsFactory problemDetailsFactory = HttpContext.RequestServices
+            .GetRequiredService<ProblemDetailsFactory>();
+        ModelStateDictionary modelState = new();
+        
+        foreach (IError error in errors)
+        {
+            string key = string.Empty;
+            
+            if (error.Metadata.TryGetValue(ErrorMetadataKeys.PropertyName, out object? propertyName))
+            {
+                key = propertyName.ToString()!;
+            }
+            
+            modelState.AddModelError(key, error.Message);
+        }
+        
+        ValidationProblemDetails validationProblemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+            HttpContext,
+            modelState
+        );
+        
+        return base.BadRequest(validationProblemDetails);
+    }
     
     #endregion
 }
