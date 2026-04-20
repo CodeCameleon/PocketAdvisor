dbdbc09eedeb9097011f41b84264af271299943b
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-04-20 07:20:34 +0200

    Created the session controller class.

---
+++ Backend/PocketAdvisor.WebApplication/Controllers/SessionController.cs
@@ -0,0 +1,48 @@
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+using PocketAdvisor.Requests.Users;
+using PocketAdvisor.Responses.Users;
+using PocketAdvisor.Services.Interfaces;
+
+namespace PocketAdvisor.WebApplication.Controllers;
+
+/// <summary>
+/// The controller responsible for handling session-related operations.
+/// </summary>
+[Route("api/sessions")]
+public sealed class SessionController
+    : BaseController<IUserService>
+{
+    #region Constructors
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SessionController" /> class.
+    /// </summary>
+    /// <param name="userService">The user service instance.</param>
+    public SessionController(IUserService userService) : base(userService) { }
+    
+    #endregion
+    
+    #region LoginAsync
+    
+    /// <summary>
+    /// Authenticates a user and issues a JSON Web Token and refresh token asynchronously.
+    /// </summary>
+    /// <param name="request">The credentials of the user to authenticate.</param>
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginRequest request)
+    {
+        Result<LoginResponse> result = await Service.LoginAsync(request);
+        
+        if (result.IsFailed)
+        {
+            return BadRequest(result.Errors);
+        }
+        
+        return StatusCode(StatusCodes.Status201Created, result.Value);
+    }
+    
+    #endregion
+}
