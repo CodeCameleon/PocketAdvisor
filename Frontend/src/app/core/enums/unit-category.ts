b17b29a82c456e62c97cd23964fa282abf8c1649
Adam <51168566+CodeCameleon@users.noreply.github.com>
2026-05-02 16:21:42 +0200

    Added the enums to the frontend.

---
+++ Frontend/src/app/core/enums/unit-category.ts
@@ -0,0 +1,10 @@
+export enum UnitCategory {
+  Uncategorized = 1,
+  Length = 2,
+  Mass = 3,
+  Area = 4,
+  Volume = 5,
+  Time = 6,
+  Energy = 7,
+  DataSize = 8,
+}
---
+++ Frontend/src/app/core/enums/unit.ts
@@ -0,0 +1,47 @@
+export enum Unit {
+  // Uncategorized
+  Piece = 1,
+
+  // Length
+  Millimeter = 101,
+  Centimeter = 102,
+  Meter = 103,
+  Kilometer = 104,
+
+  // Mass
+  Milligram = 201,
+  Gram = 202,
+  Kilogram = 203,
+  Tonne = 204,
+
+  // Area
+  SquareMeter = 301,
+  SquareKilometer = 302,
+  Acre = 306,
+  Hectare = 307,
+
+  // Volume
+  Milliliter = 401,
+  Liter = 402,
+  CubicMeter = 403,
+
+  // Time
+  Second = 501,
+  Minute = 502,
+  Hour = 503,
+  Day = 504,
+  Month = 505,
+  Year = 506,
+
+  // Energy
+  Joule = 601,
+  Kilojoule = 602,
+  KilowattHour = 603,
+
+  // DataSize
+  Byte = 701,
+  Kilobyte = 702,
+  Megabyte = 703,
+  Gigabyte = 704,
+  Terabyte = 705,
+}
